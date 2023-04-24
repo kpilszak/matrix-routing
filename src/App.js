03e0629febd2eedb905c57f9297089378c986b17
kpilszak <klaudia.pilszak@gmail.com>
2023-04-24 16:37:31 -0400

    Show traffic

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import * as tt from '@tomtom-international/web-sdk-maps';
 const App = () => {
   const mapElement = useRef()
   const [map, setMap] = useState({})
-  const longitude = -0.112869
-  const latitude = 51.504
+  const [longitude, setLongitude] = useState(-0.112869)
+  const [latitude, setLatitude] = useState(51.504)
 
   useEffect(() => {
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true
+      },
       center: [longitude, latitude],
       zoom: 14
     })
