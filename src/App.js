16e94cf6ef3bf023444737c1964638c726af533d
kpilszak <klaudia.pilszak@gmail.com>
2023-09-25 19:22:19 -0400

    Center map to London

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import * as tt from '@tomtom-international/web-sdk-maps';
 const App = () => {
   const mapElement = useRef()
   const [map, setMap] = useState({})
+  const longitude = -0.112869
+  const latitude = 51.504
 
   useEffect(() => {
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
+      center: [longitude, latitude],
+      zoom: 14
     })
 
     setMap(map)
